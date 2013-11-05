ae1f58786de1129e3431db1aba544d7de612c2e2
int32at <admin@andreas-scharf.at>
2013-11-05 23:50:36 +0100

    basic implementation of text field + people picker and specs!

--- spec/formSpec.js
+++ spec/formSpec.js
@@ -1,15 +1,71 @@
 describe('form.js Tests', function() {
-	
+
 	it("Object defined", function() {
 		expect(sponge.forms).not.toBeUndefined();
 	});
 
-	it("Find Field", function() {
-		loadFixtures('simpleTextField.html');
+	describe('Simple Text Box', function() {
 
-		var actual = $("#field");
+		var textField;
 
-		expect(actual.length).toBe(1);
+		beforeEach(function() {
+			loadFixtures('simpleTextField.html');
+			textField = sponge.forms.fields("Title");
+		});
+
+		it('should find simple text field with id = Title', function() {
+			expect(textField).not.toBeUndefined();
+			expect(textField.name).toBe("Title");
+			expect(textField.data.length).toBe(1);
+			expect(textField.type).toBe("TextField");
+		});
+
+		it('should not find simple text field without a given name', function() {
+
+			//this will fail because NotValidTitle is not included in the fixture
+			var actual = sponge.forms.fields("NotValidTitle");
+			expect(actual).toBeUndefined();
+		});
+
+		it('should add the disabled attribute when the disabled() method is called', function() {
+
+			textField.disable();
+			var disabled = textField.data.attr("disabled");
+			expect(disabled).toBe("disabled");
+		});
+
+		it('should be possible to set the value to "TEST" and get the value again', function() {
+			var value = "TEST";
+			textField.set(value);
+			expect(textField.get()).toBe(value);
+		})
 	});
 
+	describe('People Picker', function() {
+
+		var peoplePicker;
+
+		beforeEach(function() {
+			loadFixtures('peoplePicker.html');
+			peoplePicker = sponge.forms.fields("Owner");
+		});
+
+		it('should find the people picker with id = Owner', function() {
+			expect(peoplePicker).not.toBeUndefined();
+			expect(peoplePicker.name).toBe("Owner");
+			expect(peoplePicker.data.length).toBe(1);
+			expect(peoplePicker.type).toBe("upLevelDiv");
+		});
+
+		it('should not find the people picker with id = NotValidTitle', function() {
+			var actual = sponge.forms.fields("NotValidTitle");
+			expect(actual).toBeUndefined();
+		});
+
+		it('should be possible to get and set the value', function() {
+			var value = "TEST";
+			peoplePicker.set(value);
+			expect(peoplePicker.get()).toBe(value);
+		});
+	});	
 });
--- src/forms.js
+++ src/forms.js
@@ -5,20 +5,86 @@
 		var self = this;
 
 		var findField = function(name) {
-			return { "field" : "test" };
+			var type = undefined;
+
+			var f = $("input[Title='" + name + "']");
+
+			if (f.length === 0) {
+                f = $("Select[Title='" + name + "']");
+            }
+
+            //multi dropdown
+            if (f.length === 0) {
+                f = $("Select[Title='" + name + " selected values']");
+            }
+
+            //people picker
+            if (f.length === 0) {
+                var row = $("nobr:contains('" + name + "')").closest("tr");
+                f = row.find("div[name='upLevelDiv']");
+            }
+
+            //multi checkboxes
+            if (f.length === 0) {
+                var row = $("nobr:contains('" + name + "')").closest("tr");
+                f = row.find("span .ms-RadioText");
+            }
+
+            //multiline textbox
+            if (f.length === 0) {
+                var row = $("nobr:contains('" + name + "')").closest("tr");
+                f = row.find("div[role=textbox]");
+            }
+
+			if(f.length === 0) {
+				return undefined;
+			}
+
+			return new field(f, name, type);
 		};
 
-		var findAllFields = function() {
+		function field(data, name, type) {
+			var self = this;
+			self.data = data;
+			self.name = name;
+			self.type = function(type) {
 
-		};
+				if(type !== undefined)
+					return type;
+
+				var id = self.data.attr("id");
+
+                if (typeof id === "undefined")
+                	return self.type;
+
+                return id.substr(id.lastIndexOf("_") + 1, id.length);
+			}();
+
+			var get = function() {
+				return self.data.val();
+			};
+
+			var set = function(value) {
+				self.data.val(value);
+			};
+
+			var disable = function() {
+				self.data.attr("disabled", "disabled");
+			};
+
+			return {
+				name : self.name,
+				data : self.data,
+				type : self.type,
+				get : get,
+				set : set,
+				disable : disable
+			};
+		}
 
 		return {
-			field : function(name) {
+			fields : function(name) {
 				return findField(name);
-			},
-
-			fields : function() {
-				return findAllFields();
 			}
 		};
 	}();
